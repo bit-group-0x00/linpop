fa5e43c91f6f8d40e2d09d70fca45b3b5e9afa49
Amnesia <1044117498@qq.com>
2019-09-01 20:10:49 -0700

    update database module

--- include/DATABASE_mysql.h
+++ include/DATABASE_mysql.h
@@ -6,13 +6,22 @@
 #define LINPOP_DATABASE_MYSQL_H
 
 #include <mysql/mysql.h>
-#include <stdio.h>
-#include <string.h>
+
 #define HOST "127.0.0.1"
-#define USERNAME "root"
-#define PASSWORD "neusoft"
+#define USERNAME "linpop"
+#define PASSWORD "linpop"
 #define SELECT_LAST_ID "SELECT LAST_INSERT_ID();"
 typedef int Status;
 
+/**
+ * 连接数据库
+ * @return 1:MYSQL(提示success)   -1:NULL MYSQL(提示fail)
+ */
+MYSQL*  mysqlConnection();
 
+/**
+ * 关闭数据库
+ * @param connection 数据库连接句柄
+ */
+void mysqlCloseConnection(MYSQL *connection);
 #endif //LINPOP_DATABASE_MYSQL_H
