3d077f3972d5fe8d6e93889f7bcc8724c3096bee
Junqi Lu <jq_lu@outlook.com>
2019-09-02 12:03:34 +0800

    UPDATE MODULE DATABASE finish the 'group_user' table.

--- include/DATABASE_groupUser.h
+++ include/DATABASE_groupUser.h
@@ -5,23 +5,32 @@
 #ifndef LINPOP_DATABASE_GROUPUSER_H
 #define LINPOP_DATABASE_GROUPUSER_H
 
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
 #include <mysql/mysql.h>
-typedef int Status;
+#include "DATABASE_mysql.h"
 
 typedef struct {
     int groupId;
     int groupUserId;
 } GroupUser;
 
+typedef struct {
+    int userId;
+    int* groupIds;
+    int groupNum;
+} GroupList;
+
+/**
+ * free groupList.groupIds' memory
+ * @param groupList
+ */
+void freeGroupList(GroupList groupList);
+
 /**
  * 在创建群聊或添加新成员进入群聊时调用此函数
  * @param groupId 群ID
  * @param groupUserId 新成员ID
  * @param connection　链接后的句柄
- * @return true: 加入成功. false: 加入失败
+ * @return 1: 加入成功. -1: 加入失败
  */
 Status insertGroupUser(int groupId, int groupUserId, MYSQL* connection);
 
@@ -30,8 +39,16 @@ Status insertGroupUser(int groupId, int groupUserId, MYSQL* connection);
  * @param groupId 群ID
  * @param groupUserId 将要退出的成员ID
  * @param connection　链接后的句柄
- * @return true: 删除成功 false: 删除失败
+ * @return 1: 删除成功 -1: 删除失败
  */
 Status deleteGroupUser(int groupId, int groupUserId, MYSQL* connection);
 
+/**
+ *
+ * @param userId
+ * @param connection
+ * @return GroupList include the num of userId's group and their groupIds.
+ * @return when query error occur or user has no group: groupList.groupNum = 0, groupList.groupIds = NULL;
+ */
+GroupList getGroupListByUserId(int userId, MYSQL* connection);
 #endif //LINPOP_DATABASE_GROUPUSER_H
