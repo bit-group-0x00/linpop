3ac416fffacc01e4ee102369343dad92260e4187
Junqi Lu <jq_lu@outlook.com>
2019-09-02 13:45:30 +0800

    UPDATE MODULE DATABASE finish the 'groupMessage' table.

--- include/DATABASE_groupMessage.h
+++ include/DATABASE_groupMessage.h
@@ -4,16 +4,8 @@
 
 #ifndef LINPOP_DATABASE_GROUPMESSAGE_H
 #define LINPOP_DATABASE_GROUPMESSAGE_H
-#define messageMaxSize 1000
-#define dateTimeMaxSize 100
-#define SELECT_LAST_ID "SELECT LAST_INSERT_ID();"
 
-#include <stdbool.h>
-#include <mysql/mysql.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-typedef int Status;
+#include "DATABASE_mysql.h"
 
 /**
  * 这里还要添加DATETIME类型
@@ -23,16 +15,36 @@ typedef struct {
     char *gmContent;
     int gmFromId;
     int gmGroupId;
-    char* dataTime;
+    char* gmDateTime;
 } GroupMessage;
 
 typedef struct {
-    GroupMessage* gmMsgs;   //group_message表中的 messages
+    GroupMessage* gmMsgs;   //group_message表中的 msgs
     int gmNum;
 } GroupMessageList;
 
 /**
+ *
+ * @param groupMessage
+ */
+void freeGroupMessage(GroupMessage *groupMessage);
+
+/**
+ *
+ * @param groupMessageList
+ */
+void freeGroupMessageList(GroupMessageList groupMessageList);
+
+/**
+ * @param a GroupMessage pointer need to be allocated memory to
+ */
+void mallocGroupMessage(GroupMessage *groupMessage);
+
+/**
  * 在发送数据时调用此函数. 向数据库中插入这条消息.
+ *
+ * must assign: msg->gmGroupId, msg->gmContent, msg->gmFromId
+ *
  * gmId将会自动生成. gmFromId为发送者的ID, gmGroupId为接受者的ID
  * @param msg 要插入消息的指针
  * @param mysql 数据库链接句柄
@@ -46,7 +58,8 @@ int insertGmMsg(GroupMessage *msg, MYSQL* connection);
  * 数据库查询数据时要同时获得消息条数
  * @param groupId 群ID
  * @param connection　链接后的句柄
- * @return 有信息返回GroupMessageList, 包括gmMsgs数组和信息数目gmNum. 无信息时返回NULL.
+ * @return 有信息返回GroupMessageList, 包括gmMsgs数组和信息数目gmNum.
+ * @return when there is no groupList or query fail, return GroupMessageList(gmMsgs = NULL, gmNum = 0)
  */
 GroupMessageList getGmMsgList(int groupId, MYSQL* connection);
 
