9e52d732a4586794d651f2bc0a8f612520dae095
BestMt111 <543763930@qq.com>
2019-09-03 16:18:07 +0800

    Merge branch 'UI' of /home/new/CLionProjects/linpop with conflicts.

--- src/NET_client.c
+++ src/NET_client.c
@@ -32,7 +32,7 @@ state regist(const char* nick_name, const char* passwd)
     cjson = recv_cjson(server, server_buff, &server_buff_remain);
     state s =  cJSON_GetObjectItem(cjson, "type")->valueint;
     cJSON_Delete(cjson);
-    return s;  
+    return s;
 }
 
 state login(const int id, const char* passwd)
@@ -67,8 +67,8 @@ state send_msg_to_friend(const int friend_id, const char* msg, void(*callback)(s
     state s = cJSON_GetObjectItem(cjson, "type")->valueint;
 }
 
-/* 
-  client side cjson handle function 
+/*
+  client side cjson handle function
   客户端的cjson处理函数，不需要被外部调用，不用在.h文件中声名。
   实现的功能是解析cjson，并且根具cjson的类型执行相应的命令。
   在调用监听端口（monitor_port）和监听socket（monitor_socket）
@@ -81,7 +81,7 @@ void handle_cjson(int socket, cJSON* cjson)
     cJSON* type = cJSON_GetObjectItem(cjson, "type");
     switch(type->valueint)
     {
-        
+
         default:
         {
             printf("recieved unknown type message:\n");
